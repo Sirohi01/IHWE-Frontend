be87f12baec3df56595718b97ca9597407e6bbb7
RishiEncodency95 <rishi.encodency95@gmail.com>
2026-04-25 18:14:16 +0530

    seller layout create

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -110,16 +110,20 @@ const Topbar = () => {
           </span>
 
           <div className="flex items-center gap-2">
+
             <Link to="/exhibitor-login" className="px-3 py-1.5 rounded-md bg-white/5 hover:bg-[#d26019] text-white transition-all duration-300 font-bold border border-white/30 hover:border-[#d26019] text-[9px] uppercase tracking-wider whitespace-nowrap shadow-sm hover:scale-105 inline-block">
               Exhibitor Login
             </Link>
             <Link to="/buyer-login" className="px-3 py-1.5 rounded-md bg-white/5 hover:bg-[#d26019] text-white transition-all duration-300 font-bold border border-white/30 hover:border-[#d26019] text-[9px] uppercase tracking-wider whitespace-nowrap shadow-sm hover:scale-105 inline-block">
               Buyer Login
             </Link>
+            <Link to="/seller-login" className="px-3 py-1.5 rounded-md bg-white/5 hover:bg-[#d26019] text-white transition-all duration-300 font-bold border border-white/30 hover:border-[#d26019] text-[9px] uppercase tracking-wider whitespace-nowrap shadow-sm hover:scale-105 inline-block">
+              Seller Login
+            </Link>
             <button className="px-3 py-1.5 rounded-md bg-white/5 hover:bg-[#d26019] text-white transition-all duration-300 font-bold border border-white/30 hover:border-[#d26019] text-[9px] uppercase tracking-wider whitespace-nowrap shadow-sm hover:scale-105">
               Delegates Login
             </button>
-            <a 
+            <a
               href="https://admin.ihwe.in/login"
               target="_blank"
               rel="noopener noreferrer"
