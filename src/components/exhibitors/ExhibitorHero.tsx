188387d02f86c1ea48cd4b65a50e2626c7c0d580
Manish <officialmanishsirohi.01@gmail.com>
2026-05-04 17:37:09 +0530

    minor changes in Award Categories and ExhibitorCta

--- src/components/exhibitors/ExhibitorHero.tsx
+++ src/components/exhibitors/ExhibitorHero.tsx
@@ -25,7 +25,7 @@ const ExhibitorHero = () => {
             }}
         >
             {/* Subtle Gradient Overlay for text readability on left side */}
-            <div className="absolute inset-0 bg-gradient-to-r from-black/70 via-black/30 to-transparent z-10" />
+            <div className="absolute inset-0 bg-gradient-to-r from-black/40 to-transparent z-10" />
 
             <div className="relative z-20 max-w-[1400px] mx-auto px-6 md:px-12 w-full h-full flex flex-col justify-center items-start">
                 <div className="max-w-4xl">
