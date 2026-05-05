73f0749ddc4dfc038fec464342843b2c853edfb9
RishiEncodency95 <rishi.encodency95@gmail.com>
2026-05-05 18:25:00 +0530

    2nd page design working

--- src/pages/governmentMsmePmsSchemes/GovernmentMsmePmsSchemes.jsx
+++ src/pages/governmentMsmePmsSchemes/GovernmentMsmePmsSchemes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MPSchemeHero from "../../components/governmentMsmePmsSchemes/MPSchemeHero";
 import VerifyCheck from "../../components/governmentMsmePmsSchemes/VerifyCheck";
+import WhyPart from "../../components/governmentMsmePmsSchemes/WhyPart";
 
 const GovernmentMsmePmsSchemes = () => {
     return (
         <div className="bg-[#FCFCFA]">
             <MPSchemeHero />
             <VerifyCheck />
+            <WhyPart />
         </div>
     );
 };
