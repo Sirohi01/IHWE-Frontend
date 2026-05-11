f8a565de15c7a4aa9d931a0e1f201938dad6cf3c
RishiEncodency95 <rishi.encodency95@gmail.com>
2026-05-11 14:05:14 +0530

    why visit ihwe page create

---
+++ src/pages/why_visit/WhyVisit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WhyVisitHero from "@/components/why_visit/WhyVisitHero";
+
+const WhyVisit = () => {
+    return (
+        <>
+            <WhyVisitHero />
+        </>
+    )
+}
+
+export default WhyVisit;
