6d99ad5ef8cdbce2129b68c262ecb4b2f0da666d
Manish <officialmanishsirohi.01@gmail.com>
2026-05-27 18:17:37 +0530

    exhibitor adons

--- src/pages/exhibitor/ExhibitorAccessoriesPage.tsx
+++ src/pages/exhibitor/ExhibitorAccessoriesPage.tsx
@@ -1,17 +1,9 @@
 import { useExhibitorCtx } from '@/context/ExhibitorContext';
 import StallExtras from '@/components/dashboard/exhibitor/StallExtras';
-import DashboardHero from '@/components/dashboard/DashboardHero';
-
 export default function ExhibitorAccessoriesPage() {
     const { data } = useExhibitorCtx();
     return (
         <div className="space-y-6">
-            <DashboardHero 
-                pageId="ex-accessories" 
-                defaultTitle="Stall Accessories" 
-                defaultSubtitle="Order additional furniture, lights, and power sockets"
-                type="exhibitor" 
-            />
             <StallExtras data={data} />
         </div>
     );
