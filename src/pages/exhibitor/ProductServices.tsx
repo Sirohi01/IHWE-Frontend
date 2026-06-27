b87e775321982e1f287376bf99b33ef6a289f380
RishiEncodency95 <rishi.encodency95@gmail.com>
2026-06-27 15:55:50 +0530

    page recover

--- src/pages/exhibitor/ProductServices.tsx
+++ src/pages/exhibitor/ProductServices.tsx
@@ -1,22 +1,4 @@
-// import DashboardHero from "@/components/dashboard/DashboardHero";
-// import StallProductManager from "@/components/dashboard/exhibitor/StallProductManager";
-// import { useExhibitorCtx } from "@/context/ExhibitorContext";
 
-// export default function ProductServices() {
-//     const { data } = useExhibitorCtx();
-
-//     return (
-//         <div className="space-y-6">
-//             <DashboardHero
-//                 pageId="ex-products-services"
-//                 defaultTitle="My Product/Services"
-//                 defaultSubtitle="Manage your live product catalogue and enquiries"
-//                 type="exhibitor"
-//             />
-//             <StallProductManager data={data} mode="seller" initialSection="products" />
-//         </div>
-//     );
-// }
 import { useState, useMemo } from 'react';
 import {
     Plus, Layers, Users, TrendingUp,
--- src/pages/exhibitor/StallInformation.tsx
+++ src/pages/exhibitor/StallInformation.tsx
@@ -1,22 +1,4 @@
-// import DashboardHero from "@/components/dashboard/DashboardHero";
-// import StallProductManager from "@/components/dashboard/exhibitor/StallProductManager";
-// import { useExhibitorCtx } from "@/context/ExhibitorContext";
 
-// export default function StallInformation() {
-//     const { data } = useExhibitorCtx();
-
-//     return (
-//         <div className="space-y-6">
-//             <DashboardHero
-//                 pageId="ex-stall-info"
-//                 defaultTitle="Stall Information"
-//                 defaultSubtitle="Your live stall allocation, event and venue details"
-//                 type="exhibitor"
-//             />
-//             <StallProductManager data={data} mode="exhibitor" initialSection="stall-info" />
-//         </div>
-//     );
-// }
 import React from 'react';
 import {
     Download, FileText, ChevronRight, CheckCircle2, QrCode,
